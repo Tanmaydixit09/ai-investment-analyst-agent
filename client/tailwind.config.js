fdc0ff17d64ac549207fd36df4b5ec03112d6c46
Tanmay <153426958+tanmaydixt09@users.noreply.github.com>
2026-07-12 17:28:09 +0530

    style: customize Tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        ink: {
+          950: '#0B1220',
+          900: '#121A2B',
+          800: '#1B2740',
+        },
+        fog: {
+          200: '#C9D2E3',
+        },
+        paper: {
+          50: '#F3F5FA',
+        },
+        signal: {
+          amber: '#D8A73D',
+          green: '#34D399',
+          red: '#F0575A',
+          slate: '#8B96AE',
+        },
+      },
+      fontFamily: {
+        display: ['"Space Grotesk"', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
